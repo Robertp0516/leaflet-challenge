de71256fad74e7a09ae47c51db58425fc7ed5fbc
Robert A Peron <Robertp0516@gmail.com>
2020-10-28 18:18:32 -0400

    Adding api_key for mapbox and initial js code for leaflet 1

--- Leaflet-Step-1/Instructions/static/js/config.js
+++ Leaflet-Step-1/Instructions/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1Ijoicm9iZXJ0cDA1MTYiLCJhIjoiY2tmb3BndnEzMXo0ajJ6bWpleWYzaGtoNiJ9.LHhqNhzeOl5ywvWkyvdqfw";
---
+++ Leaflet-Step-1/Instructions/static/js/logic.js
@@ -0,0 +1,26 @@
+// Store our API endpoint inside queryUrl
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
+
+// Perform a GET request to the query URL
+d3.json(queryUrl, function(data){
+    createFeatures(data.features);
+});
+
+function createFeatures(earthquakeData){
+    function onEachFeature(feature, layer){
+        layer.bindPopup("<h3>" + feature.properties.place +
+        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+    }
+
+    // Create a GeoJSON layer containing the features array on the earthquakeData object
+    // Run the onEachFeature function once for each piece of data in the array
+    var earthquakes = L.geoJSON(earthquakeData, {
+        onEachFeature: onEachFeature
+    });
+    
+    createMap(earthquakes);
+}
+
+function createMap(earthquakes){
+    var streetmap = L.tileLayer()
+}
